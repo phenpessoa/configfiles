13b297e1d6dabf43a2b7bc683915741f024c45b8
phenpessoa <phenpessoa@gmail.com>
2023-12-05 18:46:17 -0300

    nvim: add ray-x/go.nvim to the list of plugins

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -131,5 +131,19 @@ local plugins = {
     },
     cmd = { "TodoTrouble", "TodoTelescope", "TodoQuickFix" },
   },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
 }
 return plugins
