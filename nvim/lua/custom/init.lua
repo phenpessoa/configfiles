fc7434003c69adacaa89f97f87690934fbc45b45
phenpessoa <phenpessoa@gmail.com>
2023-12-07 01:37:14 -0300

    nvim: add some keybindings

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -7,3 +7,6 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
+vim.api.nvim_set_keymap('n', '<leader>Y', 'gg"+yG', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
